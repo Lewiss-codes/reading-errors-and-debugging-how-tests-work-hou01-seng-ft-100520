ec5227bddd753115e375ea58156760796cb2f495
Luis Penaloza <ldp231@yahoo.com>
2020-09-06 00:08:55 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,5 +2,5 @@
 first_number = 25
 second_number = 5
 sum = first_number + second_number
-differnce = first_number - second_number
+difference = first_number - second_number
 product = first_number * second_number
